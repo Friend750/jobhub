6e009544780997fed1bfe6b6c01a7d2e41a3a27f
Hussein1423 <h.qaid14@gmail.com>
2024-11-14 10:54:40 +0300

    change in route

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Livewire\UserProfile;
+use App\Livewire\CompanyList;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function() {
     return view('welcome');
 });
 
-Route::get('/profile', UserProfile::class);
+Route::get('/', UserProfile::class);
 
 Auth::routes();
 
