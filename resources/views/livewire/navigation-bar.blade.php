20d03778591e5385da710617bf21c6489ffdefef
Hussein1423 <h.qaid14@gmail.com>
2024-11-26 18:50:26 +0300

    Refactor Chat UI with Alpine.js

--- resources/views/livewire/navigation-bar.blade.php
+++ resources/views/livewire/navigation-bar.blade.php
@@ -1,56 +1,50 @@
+<!-- Navbar -->
+<nav class="navbar navbar-light nav-bg-light w-100">
+    <div class="container-fluid">
+        <div class="nav-left d-flex justify-content-center">
+            <a class="navbar-brand" href="#">
+                <span class="logoName">YemenJob</span>
+            </a>
+            @livewire('searchbar')
+        </div>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-light nav-bg-light w-100">
+        <div class="nav-right">
 
-        <div class="container-fluid">
-            <div class="nav-left d-flex justify-content-center">
-                <a class="navbar-brand" href="#">
-                    <span class="logoName">YemenJob</span>
-                    
+            <div class="navbar-icons ms-auto">
+                <a href="/posts" wire:navigate class="nav-link d-flex flex-column align-items-center">
+                    <i class="bi bi-house-door-fill"></i>
+                    <span>Home</span>
                 </a>
-                @livewire('searchbar')
+                <a href="/Following" wire:navigate class="nav-link d-flex flex-column align-items-center">
+                    <i class="bi bi-people-fill"></i>
+                    <span>Network</span>
+                </a>
+                <a href="/JobScreen" wire:navigate class="nav-link d-flex flex-column align-items-center">
+                    <i class="bi bi-briefcase-fill"></i>
+                    <span>Jobs</span>
+                </a>
+                <a href="/chat" wire:navigate class="nav-link d-flex flex-column align-items-center">
+                    <i class="bi bi-chat-dots-fill"></i>
+                    <span>Messages</span>
+                </a>
+                <a href="/notifications" wire:navigate class="nav-link d-flex flex-column align-items-center">
+                    <i class="bi bi-bell-fill"></i>
+                    <span>Notifications</span>
+                </a>
             </div>
 
-            <div class="nav-right">
-
-                <div class="navbar-icons ms-auto">
-                    <a href="/posts" wire:navigate class="nav-link d-flex flex-column align-items-center">
-                        <i class="bi bi-house-door-fill"></i>
-                        <span>Home</span>
-                    </a>
-                    <a href="/Following" class="nav-link d-flex flex-column align-items-center">
-                        <i class="bi bi-people-fill"></i>
-                        <span>Network</span>
-                    </a>
-                    <a href="/JobScreen" class="nav-link d-flex flex-column align-items-center">
-                        <i class="bi bi-briefcase-fill"></i>
-                        <span>Jobs</span>
-                    </a>
-                    <a href="/chat" class="nav-link d-flex flex-column align-items-center">
-                        <i class="bi bi-chat-dots-fill"></i>
-                        <span>Messages</span>
-                    </a>
-                    <a href="/notifications" class="nav-link d-flex flex-column align-items-center">
-                        <i class="bi bi-bell-fill"></i>
-                        <span>Notifications</span>
-                    </a>
-
-                </div>
-
-                <div class="dropdown">
-                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <div class="d-flex flex-column align-items-center ">
-                            <i class="bi bi-person-circle"></i>
-                            <span>Profile</span>
-                        </div>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        @livewire('user-profile-card')
-                    </ul>
-                </div>
-
+            <div class="dropdown">
+                <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div class="d-flex flex-column align-items-center ">
+                        <i class="bi bi-person-circle"></i>
+                        <span>Profile</span>
+                    </div>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    @livewire('user-profile-card')
+                </ul>
             </div>
 
         </div>
-    </nav>
-
+    </div>
+</nav>
--- resources/views/livewire/notifications.blade.php
+++ resources/views/livewire/notifications.blade.php
@@ -1,5 +1,4 @@
-
-<div class="container mt-4 col-6">
+<div class="container mt-4 col-6 bg-white mb-3">
     <div class="row">
         <!-- قسم الإحصائيات -->
         <div class="col-md-3 mt-3">
@@ -30,19 +29,19 @@
                         <h5 class="card-title">Interests</h5>
                         <ul class="nav nav-tabs" id="myTab" role="tablist">
                             <li class="nav-item" role="presentation">
-                                <a class="nav-link active text-dark" id="tab1-tab" data-toggle="tab"
-                                   href="#All" role="tab" aria-controls="tab1" aria-selected="true">
+                                <a class="nav-link active text-dark" id="tab1-tab" data-bs-toggle="tab"
+                                   href="#All" role="tab" aria-controls="All" aria-selected="true">
                                    All
                                 </a>
                             </li>
                             <li class="nav-item" role="presentation">
-                                <a class="nav-link text-dark" id="tab2-tab" data-toggle="tab"
+                                <a class="nav-link text-dark" id="tab2-tab" data-bs-toggle="tab"
                                    href="#Mentions" role="tab" aria-controls="Mentions" aria-selected="false">
                                    Mentions
                                 </a>
                             </li>
                             <li class="nav-item" role="presentation">
-                                <a class="nav-link text-dark" id="tab3-tab" data-toggle="tab"
+                                <a class="nav-link text-dark" id="tab3-tab" data-bs-toggle="tab"
                                    href="#Unread" role="tab" aria-controls="Unread" aria-selected="false">
                                    Unread
                                 </a>
@@ -51,43 +50,41 @@
                         <div class="tab-content mt-3" id="myTabContent">
                             <!-- محتوى التبويبة 1 -->
                             <div class="tab-pane fade show active text-center" id="All" role="tabpanel" aria-labelledby="tab1-tab">
-                                 <!-- إشعار -->
-                        <div class="alert alert-light d-flex justify-content-between align-items-center">
-                            <div>
-                                <strong>Ashwin Bose</strong> is requesting access to Design File - Final Project.
-                                <div>
-                                    <button class="btn btn-sm blue">Accept</button>
-                                    <button class="btn btn-sm ">Decline</button>
+                                <!-- إشعار -->
+                                <div class="alert alert-light d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <strong>Ashwin Bose</strong> is requesting access to Design File - Final Project.
+                                        <div>
+                                            <button class="btn btn-sm blue" wire:click="acceptRequest">Accept</button>
+                                            <button class="btn btn-sm" wire:click="declineRequest">Decline</button>
+                                        </div>
+                                    </div>
+                                    <span>15h</span>
                                 </div>
-                            </div>
-                            <span>15h</span>
-                        </div>
-                        <!-- إشعارات أخرى -->
-                        <div class="alert alert-light">
-                            <strong>Patrick</strong> added a comment on Design Assets - Smart Tags file: "Looks perfect, send it for technical review tomorrow!"
-                            <span class="float-right">15h</span>
-                        </div>
-                        <div class="alert alert-light">
-                            <strong>Samantha</strong> has shared a file with you
-                            <span class="float-right">14h</span>
-                        </div>
-                        <div class="alert alert-light">
-                            <strong>Steve and 8 others</strong> added comments on Design Assets - Smart Tags file
-                            <span class="float-right">15h</span>
-                        </div>
+                                <!-- إشعارات أخرى -->
+                                <div class="alert alert-light">
+                                    <strong>Patrick</strong> added a comment on Design Assets - Smart Tags file: "Looks perfect, send it for technical review tomorrow!"
+                                    <span class="float-right">15h</span>
+                                </div>
+                                <div class="alert alert-light">
+                                    <strong>Samantha</strong> has shared a file with you
+                                    <span class="float-right">14h</span>
+                                </div>
+                                <div class="alert alert-light">
+                                    <strong>Steve and 8 others</strong> added comments on Design Assets - Smart Tags file
+                                    <span class="float-right">15h</span>
+                                </div>
                             </div>
                             <!-- محتوى التبويبة 2 -->
                             <div class="tab-pane fade text-center" id="Unread" role="tabpanel" aria-labelledby="tab3-tab">
                                 <div>
-                                    <h6>محتوى التبويبة الثالث</h6>
-                                    <p>هذا هو النص داخل التبويبة الثالثة</p>
+                                    <h6>No new unread notifications</h6>
                                 </div>
                             </div>
 
                             <div class="tab-pane fade text-center" id="Mentions" role="tabpanel" aria-labelledby="tab2-tab">
                                 <div>
-                                    <h6>محتوى التبويبة الثانية</h6>
-                                    <p>هذا هو النص داخل التبويبة الثانية</p>
+                                    <h6>Mentions will appear here.</h6>
                                 </div>
                             </div>
                         </div>
@@ -97,4 +94,3 @@
         </div>
     </div>
 </div>
-
