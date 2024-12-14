a71916afa06c0b41879c5d584525da9b21af02b6
Hussein1423 <h.qaid14@gmail.com>
2024-12-14 17:44:48 +0300

    changed the colors of interest and typeaccount Pages

--- resources/views/livewire/select-interests.blade.php
+++ resources/views/livewire/select-interests.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="container mt-5 col-8">
-        <div class="card shadow-sm p-4">
+        <div class="p-4">
             <h3 class="text-center mb-3">What do you want to see on <span class="logo">Yemen Jobs?</span></h3>
             <p class="text-center">Select at least 2 interests to personalize your Yemen Jobs experience. They will be visible on your profile.</p>
             
@@ -9,11 +9,11 @@
                     {{ session('error') }}
                 </div>
             @endif
-
+          <hr>
             <div class="d-flex flex-wrap justify-content-center gap-3 mt-3">
                 @foreach ($interests as $interest)
                     <button 
-                        class="btn interest-btn px-4 py-2 mb-3 mr-2 {{ in_array($interest, $selectedInterests) ? 'active' : '' }}"
+                        class="btn interest-btn py-1 mb-3 mr-2 {{ in_array($interest, $selectedInterests) ? 'active' : '' }}"
                         wire:click="toggleInterest('{{ $interest }}')">
                         {{ $interest }} <i class="fa-solid fa-plus"></i>
                     </button>
