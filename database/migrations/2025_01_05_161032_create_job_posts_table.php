f78b6f1d0d5da2c1e836f41e13a55f05b5624eb6
jabar <abdduljabbarjaloom092.com>
2025-01-16 15:59:51 +0300

    update the project

--- database/migrations/2025_01_05_161032_create_job_posts_table.php
+++ database/migrations/2025_01_05_161032_create_job_posts_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
 
             $table->foreignId('user_id')->references('id')->on('users');
 
-
             $table->string('job_title');
             $table->text('about_job');
             $table->text('job_tasks');
