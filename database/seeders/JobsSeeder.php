5d8a5777d2a30ea6c8789905903643d1afc70315
jabar <abdduljabbarjaloom092.com>
2025-03-01 23:39:58 +0300

    update project

--- database/seeders/JobsSeeder.php
+++ database/seeders/JobsSeeder.php
@@ -1,21 +1,4 @@
 <?php
-
-// namespace Database\Seeders;
-
-// use Illuminate\Database\Seeder;
-// use App\Models\JobPost;
-// use App\Models\User;
-
-// class JobsSeeder extends Seeder
-// {
-//     public function run()
-//     {
-//         // إنشاء 50 وظيفة باستخدام Factory
-//         JobPost::factory(50)->create();
-//     }
-// }
-
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
